d202f6b1d9a1edfd399aa0d1a6d6d9fbb5d95719
timonefm <timonefm@gmail.com>
2018-12-21 11:46:49 +0600

    new katas

--- Find_The_Parity_Outlier.php
+++ Find_The_Parity_Outlier.php
@@ -6,3 +6,15 @@
  * Time: 10:42 AM
  */
 
+function find($a) {
+    $odds = [];
+    $evens = [];
+    foreach ($a as $n) {
+        if ($n % 2) array_push($odds, $n);
+        else array_push($evens, $n);
+    }
+    return count($evens) === 1 ? $evens[0] : $odds[0];
+}
+
+
+
